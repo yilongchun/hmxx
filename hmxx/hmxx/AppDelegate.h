d99f3cd12438a294eba9d5205f369ab61d2b13b1
yilongchun <5115413@qq.com>
2015-01-12 10:13:27 +0000

    创建项目

---
+++ hmxx/hmxx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hmxx
+//
+//  Created by yons on 15-1-12.
+//  Copyright (c) 2015年 hmzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
